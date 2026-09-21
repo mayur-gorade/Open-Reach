edc61344c5f2354d4301619fcafd9e70aaf6d7c6
Mayur <mayurgorade411@gmail.com>
2026-09-21 14:57:38 +0530

    Implement user registration backend

---
+++ backend/src/test/java/com/openreach/openreachbackend/OpenreachbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openreach.openreachbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenreachbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
